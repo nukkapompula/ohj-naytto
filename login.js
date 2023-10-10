41452de0b8314a80a41d4ae1aa370ba7048dfa78
Arto Eskelinen <arto.eske@gmail.com>
2023-10-10 13:45:50 +0300

    funktiot järkevämmin käyttöön

--- login.js
+++ login.js
@@ -5,49 +5,49 @@ var signupWindow = document.getElementsByTagName("div")[1];
 var signupUsername = document.getElementById("signupUsername");
 var signupPassword = document.getElementById("signupPassword");
 
-document.getElementById("loginUser").addEventListener("mousedown", function(){
-    let match = 0;
+function loginUser(){
+    let matchFound = false;
     if(loginUsername.value == "" || loginPassword.value == ""){
         window.alert("Ole hyvä ja täytä molemmat kentät.");
     } else {
         fetch('http://localhost:3000/api/items')
-            .then(response => response.json())
-            .then(items => {
-                items.users.forEach(user => {
-                    if(loginUsername.value == user.name && loginPassword.value == user.password){
-                        match += 1;
-                        return;
-                    }
-                })
-                if(match == 1){
-                    loginUsername.value = "";
-                    loginPassword.value = "";
-                    window.location = "market.html";
-                } else {
-                    window.alert("Käyttäjää ei löydy tai salasana on väärä.");
-                    loginUsername.value = "";
-                    loginPassword.value = "";
+        .then(response => response.json())
+        .then(items => {
+            items.users.forEach(user => {
+                if(loginUsername.value == user.name && loginPassword.value == user.password){
+                    matchFound = true;
+                    return;
                 }
             })
+            if(matchFound == true){
+                loginUsername.value = "";
+                loginPassword.value = "";
+                window.location = "market.html";
+            } else {
+                window.alert("Käyttäjää ei löydy tai salasana on väärä.");
+                loginUsername.value = "";
+                loginPassword.value = "";
+            }
+        })
     }
-})
+}
 
-document.getElementById("createNewAccount").addEventListener("mousedown", function(){
+function createNewAccount(){
     loginWindow.style.display = "none";
     loginUsername.value = "";
     loginPassword.value = "";
     signupWindow.style.display = "block";
-})
+}
 
-document.getElementById("returnToLogin").addEventListener("mousedown", function(){
+function returnToLogin(){
     signupWindow.style.display = "none";
     signupUsername.value = "";
     signupPassword.value = "";
     loginWindow.style.display = "block";
-})
+}
 
-document.getElementById("confirmNewAccount").addEventListener("mousedown", function(){
+function confirmNewAccount(){
     if(signupUsername.value == "" || signupPassword.value == ""){
         window.alert("Ole hyvä ja täytä molemmat kentät.");
     }
-})
+}
