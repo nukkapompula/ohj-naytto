31138be245ea43c2b58e8cd9ea44bf79d1501e59
Arto Eskelinen <arto.eske@gmail.com>
2023-10-10 14:15:38 +0300

    samannimisen tilin luomisen estäminen

--- login.js
+++ login.js
@@ -4,6 +4,8 @@ var loginPassword = document.getElementById("loginPassword");
 var signupWindow = document.getElementsByTagName("div")[1];
 var signupUsername = document.getElementById("signupUsername");
 var signupPassword = document.getElementById("signupPassword");
+var confirmationWindow = document.getElementsByTagName("div")[2];
+var accountAddedText = document.getElementById("accountAddedText");
 
 function loginUser(){
     let matchFound = false;
@@ -41,13 +43,37 @@ function createNewAccount(){
 
 function returnToLogin(){
     signupWindow.style.display = "none";
+    confirmationWindow.style.display = "none";
     signupUsername.value = "";
     signupPassword.value = "";
     loginWindow.style.display = "block";
 }
 
 function confirmNewAccount(){
+    let matchFound = false;
     if(signupUsername.value == "" || signupPassword.value == ""){
         window.alert("Ole hyvä ja täytä molemmat kentät.");
+    } else {
+        fetch('http://localhost:3000/api/items')
+        .then(response => response.json())
+        .then(items => {
+            items.users.forEach(user => {
+                if(signupUsername.value == user.name){
+                    matchFound = true;
+                    return;
+                }
+            })
+            if(matchFound == true){
+                window.alert("Käyttäjänimi on varattu.");
+                signupUsername.value = "";
+                signupPassword.value = "";
+            } else {
+                confirmationWindow.style.display = "block";
+                signupWindow.style.display = "none";
+                accountAddedText.innerHTML = `Käyttäjä ${signupUsername.value} luotu!`;
+                signupUsername.value = "";
+                signupPassword.value = "";
+            }
+        })
     }
 }
