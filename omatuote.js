d706b3706b10818325b0d56b8cce53a5e9226d51
Arto Eskelinen <arto.eske@gmail.com>
2023-10-12 07:42:20 +0300

    rahamäärä näkyviin

--- omatuote.js
+++ omatuote.js
@@ -1,6 +1,19 @@
-document.getElementById("userInfo").innerHTML = `${localStorage.getItem("loggedIn")} kirjattu sisään.`;
 document.addEventListener("DOMContentLoaded", function () {
     const userItems = document.getElementById("user-items");
+    const userName = localStorage.getItem("loggedIn");
+
+    function displayInfo(){
+        fetch('http://localhost:3000/api/items')
+        .then(response => response.json())
+        .then(items => {
+            items.users.forEach(user => {
+                if(user.name == userName){
+                    document.getElementById("userInfo").innerHTML = 
+                    `${localStorage.getItem("loggedIn")} kirjattu sisään, rahaa ${user.money}€.`;
+                }
+            })
+        })
+    }
 
     function displayItems() {
         fetch('http://localhost:3000/api/items')
@@ -8,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(items => {
                 userItems.innerHTML = '';
                 items.items
-                    .filter(item => item.user === localStorage.getItem("loggedIn"))
+                    .filter(item => item.user === userName)
                     .forEach(item => {
                         const li = document.createElement("li");
                         li.textContent = `${item.name} - €${item.price} - ${item.user} `;
@@ -18,5 +31,6 @@ document.addEventListener("DOMContentLoaded", function () {
             .catch(error => console.error(error));
     }
     displayItems();
+    displayInfo();
 }
 );
