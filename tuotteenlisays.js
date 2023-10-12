73ac1cdbea34e83486553528d9b664aae558aa21
Arto Eskelinen <arto.eske@gmail.com>
2023-10-12 07:24:59 +0300

    rahat esille, käyttäjän syöttö pois

--- tuotteenlisays.js
+++ tuotteenlisays.js
@@ -1,11 +1,23 @@
-document.getElementById("userInfo").innerHTML = `${localStorage.getItem("loggedIn")} kirjattu sisään.`;
+const userName = localStorage.getItem("loggedIn");
+
+function displayInfo(){
+    fetch('http://localhost:3000/api/items')
+    .then(response => response.json())
+    .then(items => {
+        items.users.forEach(user => {
+            if(user.name == userName){
+                document.getElementById("userInfo").innerHTML = 
+                `${localStorage.getItem("loggedIn")} kirjattu sisään, rahaa ${user.money}€.`;
+            }
+        })
+    })
+}
 
 function checkInput(event){
     event.preventDefault();
 
     let nameitem = document.getElementById("inputname").value;
     let nameprice = document.getElementById("inputprice").value;
-    let userName = document.getElementById("inputuser").value;
 
     if(nameitem == ""){
         alert("Syötä tuotteen nimi!");
@@ -15,10 +27,6 @@ function checkInput(event){
         alert("Syötä tuotteen hinta!");
         return;
     }
-    if(userName == ""){
-        alert("Syötä käyttäjä!");
-        return;
-    } 
     else {
         fetch('http://localhost:3000/api/items', {
             method: 'POST',
